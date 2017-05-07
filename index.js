ce37c9d474f9aa23230445387f880bf40ca81e2a
vladan29 <vladan29@users.noreply.github.com>
2017-05-07 15:12:34 +0200

    Add test log

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const PAGE_SIZE = 10;
 let page, values, i, limit;
 
 app.get('/api', function (req, res) {
+  console.log('api triggered');
   values = [];
   page = Number(req.query.page || 0);
   i = page * PAGE_SIZE;
   limit = i + PAGE_SIZE;
   for (; i < limit; i++) {
-  	values.push(i);
+    values.push(i);
   }
   res.send(values);
 });
