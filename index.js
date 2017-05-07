2ba90eb7c26d829579bfd549a3e336adba7e1c35
vladan3629 <vladan.starcevic@gmail.com>
2017-05-07 15:01:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const PAGE_SIZE = 10;
+
+let page, values, i, limit;
+
+app.get('/api', function (req, res) {
+  values = [];
+  page = Number(req.query.page || 0);
+  i = page * PAGE_SIZE;
+  limit = i + PAGE_SIZE;
+  for (; i < limit; i++) {
+  	values.push(i);
+  }
+  res.send(values);
+});
+
+app.listen(process.env.PORT, function () {
+  console.log('Generator app started');
+});
